5ebd5670c18a268671e7f5509358062770a7a510
MattShilling <shillinm@oregonstate.edu>
2020-04-15 22:46:23 -0700

    Adding a script to run a variety of tests to get a good 'records.csv'.

---
+++ run_mc.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+cmake CMakeLists.txt
+cmake --build .
+
+./MonteCarlo 1 1
+./MonteCarlo 1 10
+./MonteCarlo 1 100
+./MonteCarlo 1 1000
+./MonteCarlo 1 10000
+./MonteCarlo 1 100000
+./MonteCarlo 1 1000000
+./MonteCarlo 1 10000000
+
+./MonteCarlo 2 1
+./MonteCarlo 2 10
+./MonteCarlo 2 100
+./MonteCarlo 2 1000
+./MonteCarlo 2 10000
+./MonteCarlo 2 100000
+./MonteCarlo 2 1000000
+./MonteCarlo 2 10000000
+
+./MonteCarlo 3 1
+./MonteCarlo 3 10
+./MonteCarlo 3 100
+./MonteCarlo 3 1000
+./MonteCarlo 3 10000
+./MonteCarlo 3 100000
+./MonteCarlo 3 1000000
+./MonteCarlo 3 10000000
+
+./MonteCarlo 4 1
+./MonteCarlo 4 10
+./MonteCarlo 4 100
+./MonteCarlo 4 1000
+./MonteCarlo 4 10000
+./MonteCarlo 4 100000
+./MonteCarlo 4 1000000
+./MonteCarlo 4 10000000
+
+./MonteCarlo 5 1
+./MonteCarlo 5 10
+./MonteCarlo 5 100
+./MonteCarlo 5 1000
+./MonteCarlo 5 10000
+./MonteCarlo 5 100000
+./MonteCarlo 5 1000000
+./MonteCarlo 5 10000000
+
+./MonteCarlo 6 1
+./MonteCarlo 6 10
+./MonteCarlo 6 100
+./MonteCarlo 6 1000
+./MonteCarlo 6 10000
+./MonteCarlo 6 100000
+./MonteCarlo 6 1000000
+./MonteCarlo 6 10000000
+
+./MonteCarlo 7 1
+./MonteCarlo 7 10
+./MonteCarlo 7 100
+./MonteCarlo 7 1000
+./MonteCarlo 7 10000
+./MonteCarlo 7 100000
+./MonteCarlo 7 1000000
+./MonteCarlo 7 10000000
+
+./MonteCarlo 8 1
+./MonteCarlo 8 10
+./MonteCarlo 8 100
+./MonteCarlo 8 1000
+./MonteCarlo 8 10000
+./MonteCarlo 8 100000
+./MonteCarlo 8 1000000
+./MonteCarlo 8 10000000
+
